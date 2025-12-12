81fa4faacb513238b9f63e1a5d4bd978869c9b11
lpj <374453156@qq.com>
2025-12-12 21:41:24 +0800

    update

--- setup_audio.sh
+++ setup_audio.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # =======================================
-# 音乐（电脑播放的）水管 -> 流入水池
+# 音乐（电脑播放的）-> 流入水池，即：采集BGM
 # 
-# 你的人声水管 -> 流入水池
+# 你的人声 -> 流入水池，即：采集人声
 # 
 # 水池有俩出水口：
 # 
-# 出水口A -> SimpleScreenRecorder 录音
+# 出水口A -> SimpleScreenRecorder 录音，即：同时录到BGM和人声
 # 
-# 出水口B -> 监听耳机
+# 出水口B -> 耳机，即：耳机里能同时听到BGM和人声
 # =======================================
 
 echo "=== 查找音频设备 ==="
@@ -25,15 +25,18 @@ echo "=== 创建虚拟混音池 ==="
 pactl unload-module module-null-sink 2>/dev/null
 pactl load-module module-null-sink sink_name=record_pool
 
+echo "=== 正在配置音频流 ==="
+
 pactl set-default-sink record_pool
 
 pactl unload-module module-loopback 2>/dev/null
 pactl load-module module-loopback source="$MIC" sink=record_pool
+
 pactl load-module module-loopback source=record_pool.monitor sink="$HP"
 
-echo "✅ 配置完成！"
+echo "✅配置完成！"
 echo "在 Simple Screen Recorder 中："
 echo "1. 勾选 'Record audio'"
-echo "2. Source 选择: 'Monitor of recor......'"
+echo "2. Source 选择: 'Monitor of record_pool......'"
 echo "3. 开始录制"
 

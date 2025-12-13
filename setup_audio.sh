ed7b2d2eb6913200d607e7f6eeccfd4561822468
lpj <374453156@qq.com>
2025-12-13 19:46:38 +0800

    update

--- setup_audio.sh
+++ setup_audio.sh
@@ -7,9 +7,9 @@
 # 
 # 水池有俩出水口：
 # 
-# 出水口A -> SimpleScreenRecorder 录音，即：同时录到BGM和人声
+# 出水口A -> 录音软件录音，即：同时录到BGM和人声
 # 
-# 出水口B -> 耳机，即：耳机里能同时听到BGM和人声
+# 出水口B -> 耳返监听，即：同时听到BGM和人声
 # =======================================
 
 echo "=== 查找音频设备 ==="
@@ -17,18 +17,19 @@ echo "=== 查找音频设备 ==="
 UGREEN_IN=$(pactl list short sources | rg input | tail -1 | awk '{print $2}')
 echo "你的输入设备：$UGREEN_IN"
 
-UGREEN_OUT=$(pactl list short sinks | rg output | tail -1 | awk '{print $2}')
-echo "你的输出设备：$UGREEN_OUT"
+PC_OUT=$(pactl list short sinks | rg "alsa_output" | head -1 | awk '{print $2}')
+echo "你的输出设备：$PC_OUT"
 
 pactl unload-module module-null-sink 2>/dev/null
 pactl unload-module module-loopback 2>/dev/null
 
 echo "=== 创建虚拟混音池 ==="
 pactl load-module module-null-sink sink_name=record_pool
-echo "=== 正在配置音频流 ==="
+echo "=== 配置音频流 ==="
+
 pactl set-default-sink record_pool
 pactl load-module module-loopback source="$UGREEN_IN" sink=record_pool latency_msec=5
-pactl load-module module-loopback source=record_pool.monitor sink="$UGREEN_OUT"
+pactl load-module module-loopback source=record_pool.monitor sink="$PC_OUT" latency_msec=5
 
 echo "✅配置完成！"
 echo "在 Simple Screen Recorder 中："
